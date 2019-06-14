4ee8b0d6007f639943dd6df408c696b17d335f29
Sergey Bochkov <bochkovsa@gmail.com>
2019-06-14 10:05:54 +0500

    ssl fix

--- src/main/java/resnyx/Answers.java
+++ src/main/java/resnyx/Answers.java
@@ -1,16 +1,17 @@
 package resnyx;
 
+import com.jcabi.http.request.JdkRequest;
+import com.jcabi.http.wire.RetryWire;
+import com.jcabi.http.wire.TrustedWire;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import resnyx.model.Update;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 @RestController
 public class Answers {
@@ -18,22 +19,24 @@ public class Answers {
     private static final Logger LOG = LoggerFactory.getLogger(Answers.class);
 
     @PostMapping("/bot/{token}")
-    public void incoming(@PathVariable String token, @RequestBody Update payload) {
+    public void incoming(@PathVariable String token, @RequestBody Update payload) throws IOException {
         LOG.info(payload.toString());
         String chatId = payload.getMessage().getChat().getId();
         outgoing(token, chatId, "Your chatId = " + chatId);
     }
 
-    private void outgoing(String token, String chatId, String message) {
+    private void outgoing(String token, String chatId, String message) throws IOException {
         LOG.info("Sending '{}' to {}", message, chatId);
-        Map<String, Object> params = new HashMap<>();
-        params.put("chat_id", chatId);
-        params.put("text", message);
-        params.put("disable_web_page_preview", true);
-        String url = String.format("https://api.telegram.org/bot%s/sendMessage", token);
-        String resp = new RestTemplate()
-                .getForEntity(url, String.class, params)
-                .toString();
+        String resp = new JdkRequest("https://api.telegram.org")
+                .uri()
+                .path(String.format("bot%s/sendMessage", token))
+                .queryParam("chat_id", chatId)
+                .queryParam("text", message)
+                .queryParam("disable_web_page_preview", "true")
+                .back()
+                .through(RetryWire.class)
+                .fetch()
+                .body();
         LOG.info(resp);
     }
 }

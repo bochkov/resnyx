b2d4d7c2aa31af8dc992ce2ec6b42c3f6ecad79c
Sergey Bochkov <bochkovsa@gmail.com>
2020-02-02 14:43:15 +0500

    implemented send photo

--- src/main/java/resnyx/methods/message/SendPhoto.java
+++ src/main/java/resnyx/methods/message/SendPhoto.java
@@ -1,13 +1,20 @@
 package resnyx.methods.message;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.apache.commons.codec.Charsets;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import resnyx.Answer;
 import resnyx.ReplyMethod;
 import resnyx.Types;
+import resnyx.model.InputFile;
 import resnyx.model.Message;
 
 /**
@@ -34,6 +41,9 @@ public final class SendPhoto extends ReplyMethod<Message> {
      */
     private String photo;
 
+    @JsonIgnore
+    private InputFile photo0;
+
     /**
      * Optional. Photo caption (may also be used when resending photos by file_id), 0-1024 characters
      */
@@ -43,6 +53,32 @@ public final class SendPhoto extends ReplyMethod<Message> {
         super(token);
         this.chatId = chatId;
         this.photo = photo;
+    }
+
+    public SendPhoto(String token, Long chatId, InputFile photo) {
+        super(token);
+        this.chatId = chatId;
+        this.photo0 = photo;
+    }
+
+    @Override
+    protected HttpEntity toHttpEntity() {
+        if (photo0 != null) {
+            MultipartEntityBuilder entity = MultipartEntityBuilder.create()
+                    .setCharset(Charsets.UTF_8);
+            for (NameValuePair pair : params()) {
+                if (!"photo".equals(pair.getName()))
+                    entity.addTextBody(pair.getName(), pair.getValue(), ContentType.TEXT_PLAIN);
+            }
+            entity.addBinaryBody(
+                    "photo",
+                    photo0.bytes(),
+                    ContentType.DEFAULT_BINARY,
+                    photo0.filename()
+            );
+            return entity.build();
+        } else
+            return super.toHttpEntity();
     }
 
     @Override

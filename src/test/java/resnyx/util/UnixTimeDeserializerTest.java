f967a2aad0af0537e918db7ae89fbb4db6035841
Sergey Bochkov <bochkovsa@gmail.com>
2024-02-25 13:36:23 +0500

    value types test

--- src/test/java/resnyx/util/UnixTimeDeserializerTest.java
+++ src/test/java/resnyx/util/UnixTimeDeserializerTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,9 +20,20 @@ class UnixTimeDeserializerTest {
     JsonParser parser;
 
     @Test
-    void test() throws IOException {
+    void testLong() throws IOException {
         UnixTimeDeserializer des = new UnixTimeDeserializer();
+        Mockito.when(parser.currentToken()).thenReturn(JsonToken.START_OBJECT);
         Mockito.when(parser.getLongValue()).thenReturn(1694670489L);
+        LocalDateTime dt = des.deserialize(parser, null);
+        LocalDateTime expected = LocalDateTime.of(2023, 9, 14, 10, 48, 9);
+        Assertions.assertThat(dt).isEqualTo(expected);
+    }
+
+    @Test
+    void testString() throws IOException {
+        UnixTimeDeserializer des = new UnixTimeDeserializer();
+        Mockito.when(parser.currentToken()).thenReturn(JsonToken.VALUE_STRING);
+        Mockito.when(parser.getValueAsString()).thenReturn("1694670489");
         LocalDateTime dt = des.deserialize(parser, null);
         LocalDateTime expected = LocalDateTime.of(2023, 9, 14, 10, 48, 9);
         Assertions.assertThat(dt).isEqualTo(expected);
